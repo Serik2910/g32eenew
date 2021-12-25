4f655687f2e8029e9918b7fe374ed173a55a644a
Serik <koblandy@inbox.ru>
2021-12-25 12:53:07 +0600

    Initial commit

---
+++ src/main/java/serik/HomeServlet.java
@@ -0,0 +1,24 @@
+package serik;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", value = "/home")
+public class HomeServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/index.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,21 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+    <link rel="stylesheet" type="text/css" href="/bs/css/bootstrap.min.css">
+
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-10 mx-auto">
+            <h1 class="text-success text-center">EE</h1>
+<%--            <br/>--%>
+<%--            <a href="${pageContext.request.contextPath}/home-servlet">home-servlet</a>--%>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
